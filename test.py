2c9ac1e3707825116be5e4962b49abe54539da49
Ahmed_Eldairy <126954726+AhmedFarag1@users.noreply.github.com>
2025-09-28 10:22:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from best_api_key.BestApiKey import add_api_key, get_and_use_best_key # ✅ This should now work
 
 # Add a new API key
-print(add_api_key("AIzaSyCNAG1b8tlOQW0DdEZBqU8Cee5D9CNqmGw"))
+print(add_api_key(""))
